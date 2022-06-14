7f265648139ebab12afd884ccd66c992a2656768
Irene <iavivas3@gmail.com>
2022-06-14 19:28:14 +0200

    Add category models for characters

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1,18 +1,19 @@
-import { CharacterModel } from "../models/character";
+import { Counselor } from "../models/counselor";
+import { Squire } from "../models/squire";
+import { Knight } from "../models/knight";
+import { King } from "../models/king";
+import { Character } from "../models/character";
 
-export async function getCharacters(): Promise<Array<CharacterModel>> {
-    return [
-        {
-            id: CharacterModel.generateId(),
-            name: "Jaime",
-            house: "Lannister",
-            isAlive: false,
-        },
-        {
-            id: CharacterModel.generateId(),
-            name: "Daenerys",
-            house: "Targaryen",
-            isAlive: false,
-        },
+export async function createCharacters() {
+    const data: Array<Character> = [
+        { ...new King("Joffrey", "Baratheon", 33, 2) },
+        { ...new Knight("Jaime", "Lannister", 33, "espada", 8) },
+        { ...new Knight("Daenerys", "Targaryen", 33, "dragones", 9) },
     ];
+    data.push({ ...new Counselor("Tyrion", "Lannister", 33, data[2]) });
+    data.push({
+        ...new Squire("Bronn", "Aguas Negras", 33, 5, data[1] as Knight),
+    });
+
+    return data;
 }
