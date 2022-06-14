669daeda654e6cdef59b93f92be24b316cb5b493
Irene <iavivas3@gmail.com>
2022-06-14 19:27:31 +0200

    Add testing of app component

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { screen } from "@testing-library/react";
+import { anyCharacter, Character } from "../../models/character";
+import { render } from "../../store/test-utils";
+import { store } from "../../store/store";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test("renders learn react link", () => {
+    const preloadedState = {
+        characters: [] as Array<anyCharacter>,
+        speaker: { ...new Character("", "", 0, "king") },
+    };
+    render(<App />, { preloadedState, store });
+    expect(screen.getByText(/Learning React - Redux/i)).toBeInTheDocument();
 });
